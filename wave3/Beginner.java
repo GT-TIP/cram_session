6616fd860af30921c3aac6056de7d749c2f136ab
jennakwon06 <jinkwon06@gmail.com>
2016-09-18 18:13:18 -0400

    adding wave 3

---
+++ wave3/Beginner.java
@@ -0,0 +1,10 @@
+/**
+ * @author: Jenna Kwon jkwon47@gatech.edu. Email if you have any questions!
+ *
+ * This code is for the workshop at Georgia Tech Technical Interview Prep Club.
+ * Slides pertaining to this module's code is at: http://bitly.com/gttip_fall2016_cram_session
+ *
+ */
+
+public class Beginner {
+}
